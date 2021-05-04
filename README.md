1ced3aeeaaa27f6861441e01b279cee9f1b3b60a
Peter Aleksander Bizjak <peter.aleksander.bizjak@ipmgroup.si>
2021-05-04 09:50:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# c2sanilist
+
+Anime browser built with AniList APIs.
---
+++ android/app/src/main/kotlin/com/peteralexbizjak/c2sanilist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.peteralexbizjak.c2sanilist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
